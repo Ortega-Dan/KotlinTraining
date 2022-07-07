ce84dd2eb7e66e1bbc2aba2932e59f5e82255f05
Dan Ortega <dan.ortega@autonomic.ai>
2022-07-07 15:55:06 -0500

    Basic functional coroutines

--- src/main/kotlin/MaRunner.kt
+++ src/main/kotlin/MaRunner.kt
@@ -1,10 +1,26 @@
 import com.kotlintest.test
+import kotlinx.coroutines.*
 import org.danort.App
 
-fun main() {
-    test()
+fun main() = runBlocking {
+    coroutineScope {
+        launch {
+            test1()
+        }
+        test2()
+    }
+}
 
-    print("message")
+suspend fun test1() {
+    delay(100)
+    println("Test 1-1")
+    delay(80)
+    println("Test 1-2")
 
-    App.oldMain(null)
+}
+
+suspend fun test2() {
+    println("  Test 2-1")
+    delay(150)
+    println("  Test 2-2")
 }
